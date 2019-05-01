c98754ec4c62e4c5d25238d786b516f42c354e55
TJ Horner <me@tjhorner.com>
2019-05-01 15:21:21 -0700

    debug stats

--- routes.go
+++ routes.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"runtime"
+
 	"github.com/julienschmidt/httprouter"
 )
 
+type statsResponse struct {
+	NumGoroutine     int
+	NumCPU           int
+	AllocMemory      uint64
+	TotalAllocMemory uint64
+}
+
+func stats(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var ms runtime.MemStats
+	runtime.ReadMemStats(&ms)
+
+	res := statsResponse{
+		NumGoroutine:     runtime.NumGoroutine(),
+		NumCPU:           runtime.NumCPU(),
+		AllocMemory:      ms.Alloc,
+		TotalAllocMemory: ms.TotalAlloc,
+	}
+
+	enc := json.NewEncoder(w)
+	enc.Encode(apiSuccess(res))
+}
+
 func getRouter(ctx *mbContext) *httprouter.Router {
 	router := httprouter.New()
+
+	router.GET("/_/stats", stats)
 
 	v1 := APIv1{ctx}
 	v1.Route(router)
